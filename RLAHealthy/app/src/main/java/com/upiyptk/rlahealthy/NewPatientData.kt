40b63b9f82880a26cec4085fedc1786e0103ae9e
nokatotedo <dwikypanduaqri22@gmail.com>
2023-06-23 01:54:36 +0700

    Remove unnecessary files
    Remove all unnecessary files

--- RLAHealthy/app/src/main/java/com/upiyptk/rlahealthy/NewPatientData.kt
+++
@@ -1,9 +0,0 @@
-package com.upiyptk.rlahealthy
-
-data class NewPatientData(
-    var id: Double? = null,
-    var heart: Int? = null,
-    var temperature: Int? = null,
-    var glucose: Int? = null,
-    var isNew: Boolean? = false
-)
--- RLAHealthy/app/src/main/java/com/upiyptk/rlahealthy/PatientEmergencyData.kt
+++
@@ -1,9 +0,0 @@
-package com.upiyptk.rlahealthy
-
-data class PatientEmergencyData(
-    var number: Long? = null,
-    var heart: Int? = null,
-    var temperature: Int? = null,
-    var glucose: Int? = null,
-    var new: Int? = 0
-)
